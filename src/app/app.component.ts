70f95c05f11e4a8089d2cdbc58bc782269c4874e
Rouzbeh Majlessi <rouz@Rouzs-MacBook-Pro.local>
2019-10-07 20:04:06 -0700

    redid without commit history or audio and video files too big

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { query, trigger, state, style, animate, transition } from '@angular/animations/src/animations';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'splash-screen-optm';
+    public audio: any;
+    public myVideo: any;
+    public muted: boolean;
+    public currentTime: number;
+    public showElement: boolean;
+    // public skipInt = false;
+    public isShown = false;
+    public show = false;
+    public buttonName: any = 'Show';
+
+    playAudio() {
+        const audio = new Audio();
+        audio.src = '../../assets/sounds/Swish.mp3';
+        audio.load();
+        audio.play();
+    }
+
+    mute() {
+        const myVideo = this.myVideo.getElementById('myVideo');
+        if (myVideo) {
+            this.myVideo.muted = false;
+        } else {
+            this.myVideo.muted = true;
+        }
+    }
+
+    delayPlay() {
+        this.myVideo.currentTime = 2;
+    }
+
 }
